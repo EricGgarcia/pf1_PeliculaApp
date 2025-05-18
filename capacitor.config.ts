e05a2aeba85a7c59bcba1b43cc2db8fc7f05940f
EricGgarcia <146622118+EricGgarcia@users.noreply.github.com>
2025-05-17 23:15:39 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pf1_PeliculaApp',
+  webDir: 'www'
+};
+
+export default config;
